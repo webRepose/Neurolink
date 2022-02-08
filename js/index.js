4973d41f583ab69845770d1f8fb54be7dd4ececb
Daniel <danilagalakov3@gmail.com>
2022-02-08 22:06:52 +0600

    burger

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 AOS.init();
+const burger = document.querySelector('.btn-menu');
+if (burger) {
+    const menuMobile = document.querySelector('.menu-mobile');
+    const logo = document.querySelector('.logo');
+    // const menu = document.querySelector('.menu-m');
+    burger.addEventListener("click", function(e) {
+        burger.classList.toggle('_active')
+        document.body.classList.toggle('_lock')
+        menuMobile.classList.toggle('_active')
+        logo.classList.toggle('logo_active')
+        // menu.classList.toggle('menu-m')
+    })
+}
